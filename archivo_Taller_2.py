c4be1da5f8185f826fe8b4207bec5cdc30f5be4c
jeronimoruedar-eng <jeronimoruedar@gmail.com>
2026-01-29 16:34:20 -0500

    Grafico

--- archivo_Taller_2.py
+++ archivo_Taller_2.py
@@ -4,5 +4,19 @@ x = 0.5
 def raiz_cuadrada(x:float) -> float:
     return x**0.5
 
-print("La raíz cuadrada de 144 es: " + str(raiz_cuadrada(144)))
+print("La raíz cuadrada de 1000 es: " + str(raiz_cuadrada(1000)))
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(1234)
+
+n = 100
+x = np.random.rand(n)
+y = np.random.rand(n)
+
+plt.hist(x, bins=10, alpha=0.6, label="x")
+plt.hist(y, bins=10, alpha=0.6, label="y")
+plt.legend()
+plt.show()
 
