d452962f80449fac0e4079b039b73a077562f488
jeronimoruedar-eng <jeronimoruedar@gmail.com>
2026-01-30 08:52:18 -0500

    Grafico 3

--- archivo_Taller_2.py
+++ archivo_Taller_2.py
@@ -5,6 +5,7 @@ def raiz_cuadrada(x:float) -> float:
     return x**0.5
 
 print("La raíz cuadrada de 1000 es: " + str(raiz_cuadrada(1000)))
+#--------------------------------------------------
 #Grafico 1
 import matplotlib.pyplot as plt
 import numpy as np
@@ -35,4 +36,17 @@ w = np.pi*np.random.randint(0,10,n)**2
 
 plt.scatter(x, y, c=z) 
 plt.scatter(x, x, s=w, alpha=0.6) 
+plt.show()
+
+#--------------------------------------------------
+#Gráfico 3
+
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(40)
+fig, ax = plt.subplots()
+ax.plot(np.random.standard_normal(1000).cumsum(),label="TLA")
+ax.plot(np.random.standard_normal(1000).cumsum(),label="APL")
+ax.plot(np.random.standard_normal(1000).cumsum(),label="MSF")
+ax.legend() #loc="upper right"
 plt.show()
